6e91f11a47fc3c8fa70b2a2feea9f06cac9d961b
hiremasa <syorikunn0901@gmail.com>
2020-08-27 23:59:24 +0900

    dp

--- ex2-3/Educational_DP_Contest/c.py
+++ ex2-3/Educational_DP_Contest/c.py
@@ -8,4 +8,4 @@ for i in range(1, N):
 	dp[i][0]=max(max(dp[i-1][1],dp[i-1][2])+abc[i][0],dp[i][0])
 	dp[i][1]=max(max(dp[i-1][0],dp[i-1][2])+abc[i][1],dp[i][1])
 	dp[i][2]=max(max(dp[i-1][0],dp[i-1][1])+abc[i][2],dp[i][2])
-print(max([-1]))
+print(max(dp[-1]))
