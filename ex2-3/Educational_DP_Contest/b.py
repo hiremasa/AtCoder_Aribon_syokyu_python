84d55d0c5720047ab3df642453d54a77ba67b322
hiremasa <syorikunn0901@gmail.com>
2020-09-02 02:57:15 +0900

    dp

--- ex2-3/Educational_DP_Contest/b.py
+++ ex2-3/Educational_DP_Contest/b.py
@@ -1,3 +1,4 @@
+
 N, K=map(int, input().split())
 h=list(map(int, input().split()))
 
@@ -12,3 +13,17 @@ for i in range(1, N):
 		dp[i]=min(dp[j]+abs(h[j]-h[i]) for j in range(start, i))
 
 print(dp[-1])
+
+import numpy as np
+N, K = map(int, input().split())
+H=np.array(list(map(int, input().split())) + [0] * K, dtype=np.int64)
+
+dp=np.full(N+K, 1e+14, dtype=np.int64)
+dp[0]=0
+for i in range(1, N):
+  dp[i:i + K]=np.minimum(dp[i:i + K], np.abs(H[i:i + K]-H[i-1])+dp[i-1])
+
+print(dp[N - 1])  # 0-indexedに変換しておく
+
+
+
