992b7414ccb085595744abe4d23b4b27fcaf5f01
hiremasa <syorikunn0901@gmail.com>
2020-09-10 06:30:19 +0900

    dp

--- ex2-3/2-3-1/ABC015d.py
+++ ex2-3/2-3-1/ABC015d.py
@@ -7,4 +7,34 @@ for i in range(N):
 	w, v = map(int, input().split())
 	dp[1:, w:] = np.maximum(dp[1:, w:], dp[:-1, :-w] + v)
 	#右辺: 前者は使わなかった時, 後者は使った時
-print(dp[-1, -1])
+print(np.max(dp))
+
+
+#愚直に書く
+W = int(input())
+N, K = map(int, input().split())
+weight = []
+value = []
+for i in range(N):
+    a, b = map(int, input().split())
+    weight.append(a)
+    value.append(b)
+#dp[i+1][k][w]:=i番目までを見てk枚使用し、重さwになった時のsum(value)の最大値
+dp=[[[0]*(W+1) for _ in range(K+1)] for _ in range((N+1))]
+for i in range(N):
+	for k in range(K+1):
+		for w in range(W+1):
+			#枚数的に選べないとき
+			if k>=K:
+				dp[i+1][k][w] = dp[i][k][w]
+			#枚数的に選べるとき
+			else:
+				if w>=weight[i]:
+					dp[i+1][k][w] = max(dp[i][k-1][w-weight[i]]+value[i], dp[i][k][w])
+				else:
+					dp[i+1][k][w] = dp[i][k][w]
+
+ans = 0
+for i in range(K + 1):
+    ans = max(ans, dp[N][i][W])
+print(ans)
