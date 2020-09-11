b0d2427bcf55f0af9313ed2267a4374505dbc904
hiremasa <syorikunn0901@gmail.com>
2020-09-11 13:47:52 +0900

    個数制限付き部分和問題

--- ex2-3/2-3-5/subsum_numlimit.py
+++ ex2-3/2-3-5/subsum_numlimit.py
@@ -26,7 +26,7 @@ print("Yes" if dp[N][K] else "No")
 
 
 
-###############蟻本###############
+###############蟻本 O(n sigma k)###############
 for i in range(N):
 	for j in range(K+1):
 		k=0
@@ -36,3 +36,30 @@ for i in range(N):
 			if dp[i+1][j]:
 				break
 print("Yes" if dp[N][K] else "No")
+
+
+###############蟻本 O(nK)###############
+#dp[i+1][j] := i番目まででjを作る際に余る最大のi番目の個数(作れない場合は-1)
+dp=[[-1]*(K+1) for _ in range(N+1)]
+dp[0][0]=0
+
+for i in range(N):
+	for j in range(1+K):
+		if dp[j]>=0:
+			dp[j]=M[i]
+		elif j<A[i] or dp[j-a[i]]<=0:
+			dp[j]=dp[j-a[i]]-1
+		else:
+			dp[j]=dp[j-a[i]]-1
+if dp[N][K]>=0:
+	print("Yes")
+else:
+	print("No")
+
+
+
+
+
+
+
+
