b1b5c65a345f3e47d9dae5fe33e8612ca2bd2552
hiremasa <syorikunn0901@gmail.com>
2020-09-12 19:03:51 +0900

    priority_que

--- ex2-4/2-4-1/ABC062d.py
+++ ex2-4/2-4-1/ABC062d.py
@@ -1,10 +1,13 @@
+
+#########################自作実装#########################
 import heapq 
 N=int(input())
 A=list(map(int, input().split()))
+A_rev = [-x for x in A[::-1]]#逆順にして各要素を-1倍する
 ans=-float("inf")
 for k in range(N, 2*N+1):
 	left=A[:k]
-	right=[-i for i in A[k:]] #各要素を-1倍する
+	right=A_rev[:(3*N-k)]
 
 	heapq.heapify(left)
 	heapq.heapify(right)
@@ -16,3 +19,34 @@ for k in range(N, 2*N+1):
 
 	ans= max(sum(left)+sum(right), ans)
 print(ans)
+
+
+
+#########################ABC解説実装#########################
+#http://prdc.hatenablog.com/entry/2017/09/14/142721
+import heapq
+N=int(input())
+A=list(map(int, input().split()))
+
+left, right = A[:N], [-1 * i for i in A[2*N:]]
+sum_left, sum_right = sum(left), sum(right)
+
+results = [0] * (N+1)
+results[0] = sum_left
+results[N] = sum_right
+
+heapq.heapify(left)
+heapq.heapify(right)
+
+for i in range(N, 2*N):
+    v = A[i]
+    p = heapq.heappushpop(left, v)
+    sum_left += v - p
+    results[i-N+1] += sum_left
+    
+for i in range(2*N-1, N-1, -1):
+    v = -1 * A[i]
+    p = heapq.heappushpop(right, v)
+    sum_right += v - p
+    results[i-N] += sum_right
+print(max(results))
